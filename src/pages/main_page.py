d80c5ccef934918b735dc841bd9ea534e1616a04
Oliver Spragg <oliver@converge.io>
2020-04-09 19:53:00 +0200

    implemented basic dashboard

---
+++ src/pages/main_page.py
@@ -0,0 +1,42 @@
+import plotly
+import dash
+import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
+
+def render(pathlist, pdh):
+	
+	#print(pdh.datetimes)
+	#print(pdh.readings)
+	
+	# set up the axes
+	axes = []
+	for p_name, p_data in pdh.readings.items():
+		ax = plotly.graph_objs.Scatter(name=p_name,
+		                               #x=[i for i, _ in enumerate(pdh.datetimes)],
+		                               x=pdh.datetimes,
+		                               y=p_data,
+		                               line = {"width" : 1.5},
+		                               mode='lines',
+		                               hoverinfo="text",
+		                               text=["<a href=\"%s\">Patient %s</a>" % (p_name, p_name) for v in p_data])
+		axes.append(ax)
+	
+	# set up the figure
+	fig = plotly.subplots.make_subplots(rows=1, cols=1,
+	                                    shared_xaxes=True,
+	                                    shared_yaxes=False,)
+	fig.update_layout(hovermode="closest")
+	
+	# add the axes to the figure
+	for ax in axes:
+		fig.append_trace(ax, 1, 1)
+	
+	# set up the graph
+	graph = html.Div([dcc.Graph(figure=fig,
+	                 id='my-figure',
+	                 responsive=True)])
+	
+	return graph
+
+
---
+++ src/pages/patient_details.py
@@ -0,0 +1,38 @@
+import plotly
+import dash
+import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
+
+def render(pathlist, pdh):
+	
+	# error if patient doesn't exist
+	if pathlist[0] not in pdh.readings.keys():
+		return html.Div([ html.H3("Patient ID not recognised") ])
+	
+	axes = []
+	p_name = pathlist[0]
+	p_data = pdh.readings[p_name]
+	ax = plotly.graph_objs.Scatter(name=p_name,
+	                               x=pdh.datetimes,
+	                               y=p_data,
+	                               line = {"width" : 1.5},
+	                               mode='lines')
+	axes.append(ax)
+	
+	# set up the figure
+	fig = plotly.subplots.make_subplots(rows=1, cols=1,
+	                                    shared_xaxes=True,
+	                                    shared_yaxes=False,)
+	#fig.update_layout(hovermode="closest")
+	
+	# add the axes to the figure
+	for ax in axes:
+		fig.append_trace(ax, 1, 1)
+	
+	# set up the graph
+	graph = html.Div([dcc.Graph(figure=fig,
+	                 id='my-figure',
+	                 responsive=True)])
+	
+	return graph
